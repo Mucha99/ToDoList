13cf3f91d22523bc96177db32835e3f251300886
Bartek Mucha <bartekmucha43@gmail.com>
2021-09-23 19:58:43 +0200

    Add function addList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './App.scss';
 
 import List from '../List/List';
+import Creator from '../Creator/Creator';
 import { pageContents, listData } from '../../data/dataStore';
+import { settings } from '../../data/dataStore';
 
 
 class App extends React.Component {
+  state = {
+    lists: this.props.list || [{...listData}],
+  }
+
+  static propTypes = {
+    title: PropTypes.string,
+    columns: PropTypes.array,
+  }
+
+  addList(title){
+    this.setState(state => ({
+      lists: [
+        ...state.lists,
+        {
+          key: state.lists.length ? state.lists[state.lists.length -1].key +1 :0,
+          ...listData,
+          title,
+        }
+      ]
+    }))
+  }
+
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List {...listData} />
+        <div className={styles.creator}>
+          <Creator 
+            text={settings.listCreatorText} 
+            action={title => this.addList(title)} 
+          />
+        </div>
+        <div>
+          {this.state.lists.map(({key, ...listData}) => (
+            <List key={key} {...listData} />
+          ))}
+        </div>
       </main>
     )
   }
